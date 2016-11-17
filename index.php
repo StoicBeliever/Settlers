a3a28abe4fa9e5e5856e4c07427b416fcd7b84b1
Hazzard13 <nathanhazz@gmail.com>
2016-11-17 17:36:29 -0400

    New Beginnings

---
+++ index.php
@@ -0,0 +1,9 @@
+<html>
+<head>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+	<script src="catan.js"></script>
+</head>
+<body>
+	<h1>Welcome to CATAN</h1>
+</body>
+</html>
