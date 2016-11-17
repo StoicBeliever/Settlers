cbf7637872fd3f43a524366ab0248708e2f48dcd
Hazzard13 <nathanhazz@gmail.com>
2016-11-17 18:18:42 -0400

    - Added script tags for our basic framework - Added the form inputs for setting a board size

--- catan.js
+++ catan.js
@@ -1,1 +1,8 @@
-console.log("Hello world of Catan");
+$(document).ready(function() {
+	$('#board-size').on('submit', function(e) {
+		e.preventDefault();
+		x = $('#x').val();
+		y = $('#y').val();
+		console.log('Setting an x and y of ' + x + ' and ' + y);
+	})
+});
